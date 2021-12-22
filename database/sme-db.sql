ef7b2ab94761d2fc9fb9554be148dedcc50d245b
unknown <edsunjcaldoza@gmail.com>
2021-12-22 12:26:11 +0800

    Database updated

--- database/sme-db.sql
+++ database/sme-db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 21, 2021 at 06:11 PM
+-- Generation Time: Dec 22, 2021 at 05:24 AM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
